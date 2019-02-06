be6491e1d6a0e30c8ca41cf064521d29e94efdcf
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-02-06 11:50:14 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,2 @@
-sudo sh -c "echo \"Acquire:http::proxy "http://10.1.0.10:9999/\"; >> /etc/apt/apt.conf.d/70debconf "
+
+echo 'Acquire:http::proxy "http://10.1.0.10:9999";' | sudo tee -a /etc/apt/apt.conf.d/70debconf
