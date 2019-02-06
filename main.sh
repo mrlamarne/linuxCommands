40be957367c675878159f030194a45529deabdac
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-02-06 11:32:16 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-sudo echo '"Acquire:http::proxy "http://10.1.0.10:9999/"; ' >> /etc/apt/apt.conf.d/70debconf 
+sudo sh -c "echo \"Acquire:http::proxy "http://10.1.0.10:9999/\"; >> /etc/apt/apt.conf.d/70debconf "
