e380f6590c79c4e9f3f61eff1c98c0acfc806e0d
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-03-13 04:43:14 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,3 +3,8 @@ echo 'Acquire:http::proxy "http://10.1.0.10:9999/";' | sudo tee -a /etc/apt/apt.
 
 //Trouver le processus qui vérouille le lock dpkg 
 sudo lsof /var/lib/dpkg/lock/
+
+
+//BSD pfSense
+//mettre le clavier en français
+kbdcontrol -l /usr/share/syscons/keymaps/fr.iso.kbd
