1a39da7c486b0bd8a9c8dd848bcfb4a79c53ed94
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-03-19 13:51:32 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-//Ajouter le proxy apt-cach
+//Ajouter le serveur apt-cache local
 echo 'Acquire:http::proxy "http://10.1.0.10:9999/";' | sudo tee -a /etc/apt/apt.conf.d/70debconf
 
 //Ajouter le proxy http pour apt
