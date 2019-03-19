7badc3934138d66c200a7128e1e9892ca12f924d
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-03-19 13:47:09 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,13 @@
 //Ajouter le proxy apt-cach
 echo 'Acquire:http::proxy "http://10.1.0.10:9999/";' | sudo tee -a /etc/apt/apt.conf.d/70debconf
 
+//Ajouter le proxy http pour apt
+echo 'Acquire:http::proxy "http://10.1.2.5:8080/";' | sudo tee -a /etc/apt/apt.conf.d/proxy
+
+
 //Trouver le processus qui vérouille le lock dpkg 
 sudo lsof /var/lib/dpkg/lock/
+
 
 
 //BSD pfSense
