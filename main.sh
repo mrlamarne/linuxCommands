f9a4723dc5b87072e14d64b2dcfe3e70bb0dfd6a
mrlamarne <45260256+mrlamarne@users.noreply.github.com>
2019-03-27 08:51:55 +0100

    mAj proxy settings
    Added the option to set proxy server for Ubuntu 18.04

--- main.sh
+++ main.sh
@@ -2,7 +2,14 @@
 echo 'Acquire:http::proxy "http://10.1.0.10:9999/";' | sudo tee -a /etc/apt/apt.conf.d/70debconf
 
 //Ajouter le proxy http pour apt
-echo 'Acquire:http::proxy "http://10.1.2.5:8080/";' | sudo tee -a /etc/apt/apt.conf.d/proxy
+//echo 'Acquire:http::proxy "http://10.1.2.5:8080/";' | sudo tee -a /etc/apt/apt.conf.d/proxy
+//Désuet/!\
+
+//Proxy http pour Ubuntu 18.04 > *
+echo 'http_proxy="http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+echo 'https_proxy="http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+echo 'ftp_proxy="http://10.1.2.5:8080/";' | sudo tee -a /etc/environment
+
 
 
 //Trouver le processus qui vérouille le lock dpkg 
@@ -13,6 +20,7 @@ sudo lsof /var/lib/dpkg/lock/
 sudo service mysql restart
 //carte réseau
 sudo service networking restart
+
 
 
 //BSD pfSense
